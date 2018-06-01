2528a7720529b3584a3ac9b13266b969e30e802a
Luiz Costa <phelip@thenets.org>
2018-06-01 03:12:38 -0300

    Dockerfile finished

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Creater virtualenv if not exist 
-if [ ! -d env/bin ]; then
+if [ ! -f env/bin/activate ]; then 
+    echo "# O virtualenv não foi encontrado."
+    echo "# Criando novo virtualenv..."
     virtualenv -p python3 env
 fi
 
@@ -9,8 +11,23 @@ fi
 source env/bin/activate
 
 # Install dependencies
-if [ -f src/pip-requirements.txt ]; then
-    pip install -r src/pip-requirements.txt
+PIP_FILE=src/pip-requirements.txt
+if [ -f $PIP_FILE ]; then
+    md5_cache_file=env/pip_md5.cache
+    touch $md5_cache_file
+    md5_cached=($(cat $md5_cache_file))
+    md5=($(md5sum $PIP_FILE))
+
+    # Install dependencies if not cached
+    if [ ! "$md5" = "$md5_cached"  ]; then
+        echo "# Instalando novas bibliotecas..."
+        pip install -r src/pip-requirements.txt
+        echo "... instalado!"
+        echo ""
+        # cache new hash
+        echo $md5 > $md5_cache_file
+    fi
+
 fi
 
 # Run main script
