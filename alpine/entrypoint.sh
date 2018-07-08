5430ca0f3ec2e5bb24854ab15d01abfc84a021b5
Luiz Costa <phelip@thenets.org>
2018-07-08 05:02:56 -0300

    Added suport to external 'run.sh' file

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# Helper: Colors
+# Main vars
+ROOT_PATH=$(pwd)
+VENV_PATH=$ROOT_PATH/.brasilio/venv
+SRC_PATH=src/
+RUN_FILE=$ROOT_PATH/.brasilio/run.sh
+PIP_FILE=$ROOT_PATH/.brasilio/requirements.txt
+
+# Colors
 COLOR_CYAN='\033[0;36m'
 COLOR_DEFAULT='\033[0m'
 
 # Creater virtualenv if not exist
-if [ ! -f env/bin/activate ]; then 
+if [ ! -f $VENV_PATH/bin/activate ]; then 
     echo -e "$COLOR_CYAN# CRIANDO VIRTUALENV$COLOR_DEFAULT"
     virtualenv -p python3 env
     echo ""
 fi
 
 # Active virtualenv
-source env/bin/activate
+source $VENV_PATH/bin/activate
 
 # Install dependencies
-PIP_FILE=./.brasilio/requirements.txt
 if [ -f $PIP_FILE ]; then
-    md5_cache_file=env/pip_md5.cache
+    md5_cache_file=$VENV_PATH/pip_md5.cache
     touch $md5_cache_file
     md5_cached=($(cat $md5_cache_file))
     md5=($(md5sum $PIP_FILE))
@@ -25,7 +31,7 @@ if [ -f $PIP_FILE ]; then
     # Install dependencies if not cached
     if [ ! "$md5" = "$md5_cached"  ]; then
         echo -e "$COLOR_CYAN# BAIXANDO DEPENDÊNCIAS$COLOR_DEFAULT"
-        pip install -r src/pip-requirements.txt
+        pip install -r $PIP_FILE
         echo ""
         # cache new hash
         echo $md5 > $md5_cache_file
@@ -34,5 +40,15 @@ if [ -f $PIP_FILE ]; then
 fi
 
 # Run main script
-cd src/
-python capture.py
+if [ -f $RUN_FILE ]; then
+    # Copy run file to root
+    cp $RUN_FILE $ROOT_PATH/run.sh
+    
+    # Run script
+    chmod +x $RUN_FILE
+    ./$RUN_FILE
+else
+    # Run default Python file
+    cd $SRC_PATH/
+    python capture.py
+fi
