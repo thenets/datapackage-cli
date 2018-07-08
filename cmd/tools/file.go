384be50e9b4cf0e0a91a0086d4762810ea947884
Luiz Costa <phelip@thenets.org>
2018-07-08 06:08:45 -0300

    Change license comments

--- cmd/tools/file.go
+++ cmd/tools/file.go
@@ -1,16 +1,24 @@
 // Copyright © 2018 Luiz Felipe F M Costa <luiz@thenets.org>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// 
+// MIT License
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 package tools
 
@@ -20,6 +28,8 @@ import (
 	"os"
 )
 
+// CopyFile copy static files.
+// Example: CopyFile("/tmp/source.txt", "/tmp/destiny.txt")
 func CopyFile(srcPath string, destPath string) {
 	srcFile, err := os.Open(srcPath)
 	check(err)
@@ -38,7 +48,7 @@ func CopyFile(srcPath string, destPath string) {
 
 func check(err error) {
 	if err != nil {
-		fmt.Println("Error : %s", err.Error())
+		fmt.Printf("Error : %s\n", err.Error())
 		os.Exit(1)
 	}
 }
